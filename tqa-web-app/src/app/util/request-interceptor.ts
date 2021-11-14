aa3d4f538261d0a18ea8d844bdcd89064164ea66
Edward Lach <edwarddlach@gmail.com>
2021-11-13 19:09:12 -0500

    Updated host url for http requests

--- tqa-web-app/src/app/util/request-interceptor.ts
+++ tqa-web-app/src/app/util/request-interceptor.ts
@@ -1,6 +1,7 @@
-import {Injectable} from "@angular/core";
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {Observable} from "rxjs";
+import { Injectable } from "@angular/core";
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from "../../environments/environment";
 
 
 // Will be useful later when we add authentication to our API
@@ -8,6 +9,7 @@ import {Observable} from "rxjs";
 export class RequestInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    req = req.clone({ url: environment.api_url + req.url });
     console.log(req);
     return next.handle(req);
   }
