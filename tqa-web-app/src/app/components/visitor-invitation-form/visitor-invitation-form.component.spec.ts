76895578dae72438f1bee48b12e3b8e5096e3485
Edward Lach <edwarddlach@gmail.com>
2022-03-12 14:50:34 -0500

    Added necessary dependencies to test cases for header and visitor components

--- tqa-web-app/src/app/components/visitor-invitation-form/visitor-invitation-form.component.spec.ts
+++ tqa-web-app/src/app/components/visitor-invitation-form/visitor-invitation-form.component.spec.ts
@@ -1,19 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VisitorInvitationFormComponent } from './visitor-invitation-form.component';
+import {initialPredictionState} from "../../state/store/resources/prediction/prediction.reducer";
+import {mockPredictionResponseV2} from "../../dtos/v2/mock/prediction.dto.v2.mock";
+import {initialFormState} from "../../state/store/prediction-form/prediction-form.reducer";
+import {initialVisitorState} from "../../state/store/resources/visitor/visitor.reducer";
+import {mockVisitorResponseV2} from "../../dtos/v2/mock/visitor.dto.v2.mock";
+import {MockStore, provideMockStore} from "@ngrx/store/testing";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 describe('VisitorInvitationFormComponent', () => {
   let component: VisitorInvitationFormComponent;
   let fixture: ComponentFixture<VisitorInvitationFormComponent>;
+  let store: MockStore;
+  const initialState = {
+    predictions: {...initialPredictionState, resource: mockPredictionResponseV2()},
+    predictionForm: initialFormState,
+    visitors: {...initialVisitorState, resource: mockVisitorResponseV2()}
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ VisitorInvitationFormComponent ]
+      declarations: [ VisitorInvitationFormComponent ],
+      imports: [
+        ReactiveFormsModule,
+        FormsModule
+      ],
+      providers: [
+        provideMockStore({ initialState })
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(VisitorInvitationFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
