aa49084b80e398423fd80a7e77ecdfa794cf63e4
Edward Lach <edwarddlach@gmail.com>
2022-04-11 10:47:52 -0400

    Restoring visitor token from local storage if not present in url, redirecting to main component if unauthorized access to admin pages is attempted

--- tqa-web-app/src/app/util/logged-in-admin-guard.ts
+++ tqa-web-app/src/app/util/logged-in-admin-guard.ts
@@ -1,18 +1,39 @@
 import { Injectable } from "@angular/core";
-import { CanActivate } from "@angular/router";
+import {
+  ActivatedRoute,
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree
+} from "@angular/router";
 import { AppState } from "../state/store/app.state";
 import { Store } from "@ngrx/store";
 import { isAuthenticated } from "../state/store/resources/adminauth/adminauth.selector";
+import {AppRoute} from "../constants/app-route.constant";
+import {Observable} from "rxjs";
 
 
 @Injectable()
 export class LoggedInAdminGuard implements CanActivate {
 
-  constructor(private store$: Store<AppState>) {}
+  constructor(
+    private store$: Store<AppState>,
+    private router: Router
+  ) {}
 
-  canActivate(): boolean {
-    let canActivate: boolean = false;
-    this.store$.select(isAuthenticated).subscribe(auth => canActivate = auth);
-    return canActivate;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+      Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree
+  {
+    let isAuthorized: boolean = false;
+      this.store$.select(isAuthenticated).subscribe(auth => isAuthorized = auth);
+      if (!isAuthorized) {
+        let urlTree: UrlTree = this.router.parseUrl(AppRoute.ROOT.getRoute);
+        urlTree.queryParams = state.root.queryParams;
+        return urlTree;
+      } else {
+        return true;
+      };
   }
+
 }
